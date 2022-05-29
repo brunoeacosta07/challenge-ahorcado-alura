bc972016a827276245854a3f34a03a06479f42e7
Bruno Acosta <brunoeacosta07@gmail.com>
2022-05-29 19:37:05 -0300

    Agregadas todas las funciones JS y PopUp final de juego

--- js/main.js
+++ js/main.js
@@ -1,12 +1,31 @@
-var lista_imagenes = [];
-var imagen_ahorcado = document.getElementById('imagen-ahorcado');
+// Area agregar nueva palabra
+var boton_agregar = document.getElementById('btn-guardar');
+var input_palabra = document.getElementById('input-palabra');
 
-for(let i=0; i<7;i++){
-  lista_imagenes[i] = './img/ahorcado-'+i+'.png'; 
+
+function mostrar_popup(mensaje){
+  var popup = document.getElementById('myPopup');
+  popup.textContent = mensaje;
+  popup.classList.add('show');
+  setTimeout(function(){
+    popup.classList.remove('show');
+  }, 4000);
 }
 
+boton_agregar.addEventListener('click', function(){
+  let palabra_ingresada = input_palabra.value;
 
-
-
+  if(palabra_ingresada == 0){
+    mostrar_popup('Debe ingresar una palabra');
+  }else if(palabra_ingresada.length > 8){
+    mostrar_popup('La palabra no puede contener mas de 8 caracteres');
+  }else if((/\d/.test(palabra_ingresada)) || (palabra_ingresada.includes(' '))){ // 
+    mostrar_popup('La palabra no puede contener numeros o espacios en blanco');
+  }else{
+    // palabras.push(palabra_ingresada.toUpperCase());
+    localStorage.setItem('palabra', palabra_ingresada.toUpperCase());
+    window.location = 'juego.html';
+  }
+});
 
 
